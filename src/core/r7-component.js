105193245296ce032b2d6be3e7c52dff6ffd0f51
rmmoreno <rmmoreno@r7.com>
2015-02-03 16:09:50 -0200

    JsDoc e template inicial para a documentacao

--- src/core/r7-component.js
+++ src/core/r7-component.js
@@ -1,9 +1,31 @@
-;(function (root) {
-  'use strict';
-
-  var components = {},
-  R7 = {
-    register: function (name, fn) {
+(function(root) {
+'use strict';
+/**
+ * Definicao da classe
+ * @constructor components
+ * @class
+ */
+var components = {},
+/**
+ * @name R7
+ * @namespace
+ */
+R7 = {
+/**
+ * Comment of register
+ *
+ * @method components#register
+ *
+ * @param {string} name subdescription.
+ * @param {function} fn subdescription.
+ * @example
+ *
+ * R7.register('App',{});
+ *
+ * App.function.methods blah blah
+ *
+ */
+    register: function(name, fn) {
 
       if (typeof R7[name] === 'undefined') {
         R7[name] = fn;
@@ -13,7 +35,17 @@
       throw new Error('R7: Cannot register method ' + name + '. Method already exists.');
     },
 
-    component: function (name, fn, context) {
+/**
+ * Comment of component
+ *
+ * @method components#component
+ *
+ * @param {string} name - subdescription.
+ * @param {function} fn - subdescription.
+ * @param {function} context - subdescription.
+ * @return {function} subdescription.
+ */
+    component: function(name, fn, context) {
 
       if (!components[name]) {
         components[name] = {
@@ -34,7 +66,7 @@
     }
   };
 
-  R7.helpers = R7.helpers || {};
-  root.R7 = R7;
+ R7.helpers = R7.helpers || {};
+ root.R7 = R7;
 
 } (window));
