33c60e14398eb96b98059bf50b3224ae41a9e392
Thiago Paixão <tspaixao@sp.r7.com>
2015-05-29 14:00:04 -0300

    finalizado teste

--- specs/core/r7ComponentSpec.js
+++ specs/core/r7ComponentSpec.js
@@ -1,27 +1,82 @@
-describe('#R7.component', function () {
+describe('R7',function(){
 
-  it('Returns another Object with the given name', function () {
+  describe('#component', function () {
+    'use strict';
 
-    var componentKeys = ['Models', 'Collections', 'Views', 'Routes', 'Templates'],
+    it('Returns another Object with the given name as first parameter', function () {
+
+      var defaultComponent = {
+        Models: {},
+        Collections: {},
+        Views: {},
+        Routes: {},
+        Templates: {}
+      },
       component = R7.component('Example');
 
-    expect(Object.keys(component)).toEqual(componentKeys);
+      expect(component).toEqual(jasmine.objectContaining(defaultComponent));
 
-  });
+    });
 
-  describe('Callback', function(){
+    describe('Callback', function (){
 
-    it('Calls callback when a function is given and the first argument is the component created', function(done){
+      it('Calls callback when a function is given as the second argument', function (){
 
-      R7.component('Example', function(App){
+        var obj = {
+          callbackFn : function (App) {
+            expect(App).toBeDefined();
+          }
+        };
 
-        expect(App).toBeDefined();
+        spyOn(obj, "callbackFn");
 
-        done();
+        R7.component('Example', obj.callbackFn);
+
+        expect(obj.callbackFn).toHaveBeenCalled();
+
+      });
+
+      it('Defines the context as the third argument', function (){
+
+        var self = { property : 'value'};
+
+        R7.component('Example', function() {
+
+          expect(this.property).toEqual(self.property);
+
+        }, self);
       });
 
     });
 
   });
 
+  describe('#register',function(){
+    var fnName = 'Example';
+    var fn = function(){};
+
+    afterEach(function(){
+      R7[fnName] = undefined;
+    });
+    it('Register function with the given name in the R7 namespace',function(){
+
+      R7.register(fnName, fn);
+
+      expect(R7[fnName]).toEqual(fn);
+
+    });
+
+    it('Throws an error when the function already exists', function(){
+
+      var exceptionFn = function(){
+        R7.register(fnName, fn);
+      },
+      msgError = 'R7: Cannot register method ' + fnName + '. Method already exists.';
+
+      R7.register(fnName, function(){});
+
+      expect(exceptionFn).toThrowError(msgError);
+
+    });
+  });
 });
